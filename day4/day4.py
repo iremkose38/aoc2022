eead7982a5aa967fdd45fe37f0816f28ca254bcc
iremkose38 <iremkose-038@outlook.com>
2022-12-04 21:32:57 +0100

    day 4; part 1

---
+++ day4/day4.py
@@ -0,0 +1,46 @@
+with open('input.txt', 'r') as temp_file:
+    assigments = temp_file.read().splitlines()
+    # example of input
+    # ['75-76,18-75', '2-54,1-50', '82-83,78-82', ...]
+
+
+def puzzle(lst):
+    # check per duo if one contains another, increase pairs
+    pairs = 0
+    for section in lst:
+        # 75-76,18-75
+        divided = section.split(',')
+        if contains(divided):
+            # if they contain one another: a containing pair is found
+            pairs += 1
+    return pairs
+
+
+def contains(elves):
+    # ['75-76', '18-75']
+    # add numbers in range
+    all_ranges = []
+    for elf in elves:
+        ranges = set()
+        section_id = elf.split('-')
+        # fill in missing numbers between start n end point
+        start = int(section_id[0])
+        end = int(section_id[1])
+        # added bcs of range? ranges.append(start)
+        for number in range(start, end+1):
+            ranges.add(number)
+        all_ranges.append(ranges)
+
+    # check if one FULLY contains another
+    if all_ranges[0].issubset(all_ranges[1]) or all_ranges[1].issubset(all_ranges[0]):
+        # set contains another if subset
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    solution = puzzle(assigments)
+    print('the answer is: ', solution)
+    test = ['2-4,6-8', '2-3,4-5', '5-7,7-9', '2-8,3-7', '6-6,4-6', '2-6,4-8']
+    assert (puzzle(test) == 2)
