ecd5f1f8dfd47d3501a93cd168e53cbf302d9005
iremkose38 <iremkose-038@outlook.com>
2022-12-06 16:23:34 +0100

    day 6: part one

--- day6/day6.py
+++ day6/day6.py
@@ -7,32 +7,50 @@ with open('input.txt', 'r') as temp_file:
 # How many characters need to be processed before the first start-of-packet marker is detected?
 
 def puzzle(lst):
-    # start of a packet is indicated by a sequence of four characters that are all different
-    marker = []
-    before_marker = ""
+    # start of a packet = substring of length 4 without repeating char
+    marker = []     # substring
     for signal in lst:
+        processed = 0  # count letters that have been processed until marker is found
         for letter in signal:
-            if letter not in marker:
-                marker.append(letter)
-                # when marker of len 4 is found, return amount of characters before marker
-                if len(marker) == 4:
-                    # processed before + currently processed
-                    processed = len(before_marker) + len(marker)
+            # add letter either way to marker, then check if marker is valid
+            marker.append(letter)
+            processed += 1
+            if len(marker) == 4:
+            # if len correct check for duplicates
+                if check(marker):
+                    # if marker is valid, no more checking other char
                     return processed
-            else:
-                # marker rule broken so search for new substring#
-                marker.append(letter)
-                # move rulebreakers to before marker so marker is clean to use again
-                # TO DO check if equal letters are in marker;
+                else:
+                    # new marker starting point needed
+                    marker = new_marker(marker)
 
+
+def check(mrk):
+    # check if given marker is valid
+    doubles = [letter for letter in mrk if mrk.count(letter) > 1]
+    if len(doubles) == 0:
+        return True
+    # if above is not correct, False
+    return False
+
+def new_marker(mrk):
+    # return new marker that is valid to start again for a search
+    # nppdvjthqldpwncqszvftbrmjlhg
+    new = mrk
+    # delete letters that make mrk not valid = doubles?
+    while not check(new):
+        new.pop(0)
+    return new
 
 
 if __name__ == '__main__':
-    # solution = puzzle(datastream)
-    # print("solution of the puzzle: ", solution)
+    solution = puzzle(datastream)
+    print("solution of the puzzle: ", solution)
+
     #TESTS
+    #OK
     example = ['bvwbjplbgvbhsrlpgdmjqwftvncz']
-    assert(puzzle(example) == 5)
+    # assert(puzzle(example) == 5)
 
     example2 = ['nppdvjthqldpwncqszvftbrmjlhg']
     assert(puzzle(example2) == 6)
